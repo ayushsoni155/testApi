2dd8dc7842baa7419b2bc8e0ce5ba7f09dccbb08
Ayush soni <ayushsoni.6997@gmail.com>
2026-02-03 17:59:47 +0530

    added a post api to add quote

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
+const quotes=[];
 app.get("/", (req, res) => {
   res.send("Hello, user");
 });
+
+app.post("/quote",(req,res)=>{
+  const data=req.body;
+  quotes.push(data);
+  res.status(201).json({
+    message:"Quote revived successfully!",
+    data:data
+  })
+})
 
 module.exports = app;
