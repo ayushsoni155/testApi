22876e34bb30837b0fa88fa82844dbf89807a3b7
Ayush soni <ayushsoni.6997@gmail.com>
2026-02-03 18:14:54 +0530

    added a get api to recive a random Quote

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const quotes=[];
+const quotes = [];
 app.get("/", (req, res) => {
   res.send("Hello, user");
 });
 
-app.post("/quote",(req,res)=>{
-  const data=req.body;
+app.get("/quote", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+   res.status(200).json({
+    message: "Quote retrieved successfully!",
+    data: randomQuote
+  });
+});
+
+app.post("/quote", (req, res) => {
+  const data = req.body;
   quotes.push(data);
   res.status(201).json({
-    message:"Quote revived successfully!",
-    data:data
-  })
-})
+    message: "Quote revived successfully!",
+    data: data,
+  });
+});
 
 module.exports = app;
